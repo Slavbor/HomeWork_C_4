aea57952e8442be3d7d954d6155e71ee9cb5e0ab
Slavbor <shipmate@mail.ru>
2023-01-15 02:44:40 +0300

    Zadacha 25 & 27 ready

--- Zadacha_25/Program.cs
+++ Zadacha_25/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Input numbers: ");
+
+long num1 = long.Parse(ReadLine()!);
+long num2 = long.Parse(ReadLine()!);
+
+long pow1 = GetPow(num1,num2);
+
+WriteLine($"{num1} to the power of {num2}  = {pow1}");
+
+
+
+
+
+
+
+
+long GetPow(long a, long b)
+{
+
+    long pow = 1;
+    
+    for(long i = 1; i <= b; i++)
+    {
+        pow *= a;
+    }
+    return pow;
+
+}
--- Zadacha_27/Program.cs
+++ Zadacha_27/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Input number: ");
+
+long num = long.Parse(ReadLine()!);
+
+long summary = GetSumNum(num);
+
+WriteLine($"Summary digits of {num}  = {summary}");
+
+
+
+
+
+
+
+
+long GetSumNum(long a)
+{
+    long sum = 0;
+
+    while (a != 0)
+    {
+        long b = a % 10;
+        sum += b;
+        a /= 10;
+    }
+    return sum;
+}

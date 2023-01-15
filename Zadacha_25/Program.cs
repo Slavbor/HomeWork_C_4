4915a602ad310a4e1a5311104e66cd474c469f7c
Slavbor <shipmate@mail.ru>
2023-01-16 00:55:44 +0300

    Zadacha 29 ready

--- Zadacha_25/Program.cs
+++ Zadacha_25/Program.cs
@@ -1,4 +1,5 @@
 ﻿using static System.Console;
+Clear();
 
 WriteLine("Input numbers: ");
 
--- Zadacha_29/Program.cs
+++ Zadacha_29/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+
+
+PrintArray(GetArray(8, 0, 100));
+
+
+
+
+ int[] GetArray(int size, int RandomFrom, int RandomTo)
+ {
+    int[] array = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(RandomFrom, RandomTo + 1);
+    }
+    return array;
+ }
+
+
+void PrintArray(int[] arr)
+{
+    for(int i =0; i < arr.Length; i++)
+    {
+        Write ($"{arr[i]}, ");
+    }
+    WriteLine();
+}
